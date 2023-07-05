0ce3b89a2ae6eaeabe9a6dfa574bdf9326dc4dec
tanmay patil <tanmaypatiltp25@gmail.com>
2023-07-05 16:41:24 +0530

    changes

--- next.config.js
+++ next.config.js
@@ -1,7 +1,19 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    output:"export",
-    basePath: '/ShuffleX',
+const isGithubActions = process.env.GITHUB_ACTIONS || false
+
+let assetPrefix = ''
+let basePath = '/'
+
+if (isGithubActions) {
+  // trim off `<owner>/`
+  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
+
+  assetPrefix = `/${repo}/`
+  basePath = `/${repo}`
 }
 
-module.exports = nextConfig
+module.exports = {
+    output:'export',
+  assetPrefix: assetPrefix,
+  basePath: basePath,
+}
